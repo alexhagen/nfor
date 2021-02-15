909b0b0af1b4083e6a62bda68c5d23459125156c
Alex Hagen <alexander.hagen@pnnl.gov>
2021-02-15 08:45:10 -0800

    Documented and linted

--- nfor/__init__.py
+++ nfor/__init__.py
@@ -1,8 +1,15 @@
+"""NFor module documentation."""
 import itertools
 from multiprocessing import Pool
 
-class nfor(object):
+class NFor:
+    """``NFor`` - An iterator for nested for loops (high dimensional grids)."""
     def __init__(self, variables):
+        """Initialize the ``nfor`` object``.
+
+        :param list variables: A list of arrays/lists, where every combination
+            of every element is desired in the high dimensional grid.
+        """
         self.combinations = list(itertools.product(*variables))
         self.len = len(self.combinations)
         self.counter = 0
@@ -25,16 +32,26 @@ class nfor(object):
     def __getitem__(self, idx):
         return self.combinations[idx]
 
-class NForPool(nfor):
+class NForPool(NFor):
+    """```NForPool`` object - to run nested for loops on ``multiprocessing``."""
     def __init__(self, variables, function, n_cpu=4):
+        """Initialize the ``NForPool`` object.
+
+        :param list variables: List of variables to pass to ``NFor``.
+        :param func function: Function to run on every combination.
+        :param int n_cpu: Number of CPUs to use in the process pool.
+        """
         super().__init__(variables)
         self.function = function
         self.pool = Pool(processes=n_cpu)
 
     def __call__(self):
         ress = []
+        results = []
         for job in self:
             res = self.pool.apply_async(self.function, args=job)
             ress.append(res)
         for res in ress:
-            res.get()
+            result = res.get()
+            results.append(result)
+        return results
